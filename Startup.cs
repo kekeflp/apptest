3a225a3a3a3533eeebed4c7c99ab710124ecea95
Lp Fu <kekeflp@hotmail.com>
2020-12-17 12:30:20 +0800

    v1.1

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace apptest
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World-v1.1 !");
                 });
             });
         }
